3988f035a39535b76011a188abb45ca3771c2549
netanel tesfay <netaneltes66@gmailgit config --global user.email you@example.com.com>
2022-11-17 22:43:51 +0200

    1

--- backend/routers/userRouter.js
+++
@@ -1,6 +0,0 @@
-const usersRouter = require("express").Router()
-const {createUser,createPassword} = require("../controllers/user-ctrl")
-
-usersRouter.post("/postUser", createUser)
-usersRouter.post("password/",createPassword)
-module.exports = usersRouter;
